2ef310001ea83cbee11f6a28b1276e46c3618c26
Syed Hussain Hamad Naqvi <100157373+HussainSyed268@users.noreply.github.com>
2024-06-06 15:17:33 +0500

    modf:role updated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,7 +9,7 @@ exports.register = async (req, res) => {
     try {
         const { username, password, email } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await User.create({ username, password_hash: hashedPassword, email, role: 'buyer'});
+        const newUser = await User.create({ username, password_hash: hashedPassword, email, role: 'customer'});
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
         res.status(500).json({ error: 'Failed to register user' });
